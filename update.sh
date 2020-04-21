65a3befd5045c1cfe4ffb9ce7b786d5feb4f23ce
Kazanami <6745680+Kazanami@users.noreply.github.com>
2020-04-21 14:29:34 +0900

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git remote set-url origin "https://Kazanami:${GITHUB_TOKEN}@github.com/Kazanami/git-action-tester"
+git config user.name "Github Action Runner"
+git config user.email "github-action-runner@example.com"
+git add .
+git commit -m "recursive added"
+GIT_COMMIT_ERROR=$?
+if [[ $GIT_COMMIT_ERROR != 0 ]];then
+  echo "No Update";
+  exit 0;
+fi
+git push origin master
